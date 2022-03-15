1c93a93a16f342c9ca674acda4ab692dfa4292b4
Vijay Kumar <futureinjava@gmail.com>
2022-03-15 17:57:29 +0530

    array example

---
+++ Main2.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		
+		//int num1,num2,num3,num4,num5;
+		
+		
+		 int num[]={74,56,92,32,18,67}; //initialization of array
+		
+		// int num[]; num={74,56,92,32,18,67}; //initialization of array
+		
+		
+		
+		  for(int k=0;k<num.length;k++) { 
+			  System.out.println(num[k]); 
+			  }
+		
+		
+		//Scanner in=new Scanner(System.in);
+		
+		//int num[]=new int[5];
+		
+		
+		//num[0]=in.nextInt();
+		
+//		
+//		num[0]=90; //element
+//		num[1]=37;
+//		num[2]=3;
+//		num[3]=7;
+//		num[4]=45;	
+//		
+		
+
+		
+		
+		//System.out.println(num[12]);
+		
+		/*
+		System.out.println(num[0]);
+		System.out.println(num[1]);
+		System.out.println(num[2]);
+		System.out.println(num[3]);
+		System.out.println(num[4]);
+		*/
+		
+		/*
+		  for(int k=0;k<=5;k++) { 
+			  System.out.println(num[k]); 
+			  }
+		 */
+		
+		
+		/*
+		 * for(int k=0;k<num.length;k++) { System.out.println(num[k]); }
+		 */
+		
+		
+		/*
+		double num2[];//num2 is reference variable
+		num2=new double[7];
+			*/
+		
+		
+		
+		
+		
+		
+		}
+
+	}
+
+

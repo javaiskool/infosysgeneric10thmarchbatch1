4469cc9afa0939417b514b4f259724b0627fea74
Vijay Kumar <futureinjava@gmail.com>
2022-03-15 16:22:57 +0530

    first commit

---
+++ Main1.java
@@ -0,0 +1,15 @@
+
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		int num[]= {10,20,30};
+		
+		for(int k=0;k<num.length;k++)
+			System.out.println(num[k]);
+		
+		}
+
+	}
+
+
